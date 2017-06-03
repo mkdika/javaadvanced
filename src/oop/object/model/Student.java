96d825f946d1c52a38d4f76e3cec6ba360546e07
Maikel Chandika <mkdika@gmail.com>
2017-06-03 09:36:19 +0700

    first init.

---
+++ src/oop/object/model/Student.java
@@ -0,0 +1,35 @@
+package oop.object.model;
+
+public class Student {
+    
+    private final String studentId;
+    private final String studentName;
+    private Double score;
+    
+    // Default Constructor
+    public Student() {
+        this("-","-");
+    }
+            
+    // Custom Constructor
+    public Student(String studentId, String studentName) {        
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }        
+}
